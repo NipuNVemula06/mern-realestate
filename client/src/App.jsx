715a160a0360ce383b5bce947fe65042c5187322
NipuNVemula06 <nipunvemula4@gmail.com>
2023-12-08 15:33:44 +0530

    created Header components and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Header } from "./components";
+import { About, Home, Profile, SignIn, SignUp } from "./pages";
 
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
