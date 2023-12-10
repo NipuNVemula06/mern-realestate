060929bdd8ab6395273a11a0121d991e5dc0f2f1
NipuNVemula06 <nipunvemula4@gmail.com>
2023-12-10 13:20:51 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,6 +12,36 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created Successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    // findOne method is a mongodb method to find the information in database (in this case its email)
+    const validUser = await User.findOne({ email });
+
+    // validate User
+    if (!validUser) return next(errorHandler(404, "User not found!!"));
+
+    // validate Password
+    //compareSync will decrypt the password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credential!"));
+    // token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    // remove password info before sending to client
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
